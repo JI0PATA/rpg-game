453de3ab5e8c8385a94eae8570e986f3c973b207
JI0PATA <wanja.shukov14@gmail.com>
2021-07-15 18:12:23 +0300

    Отрисовка по конфигу мира Перемещение персонажа согласно конфигу

--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -2,17 +2,24 @@ import ClientEngine from './ClientEngine';
 import sprites from '../configs/sprites';
 import ClientWorld from './ClientWorld';
 import levelCfg from '../configs/world.json';
+import gameObjects from '../configs/gameObjects.json';
 
 class ClientGame {
   constructor(cfg) {
     Object.assign(this, {
       cfg,
+      gameObjects,
+      player: null,
     });
 
     this.engine = this.createEngine();
     this.map = this.createWorld();
 
     this.initEngine();
+  }
+
+  setPlayer(player) {
+    this.player = player;
   }
 
   createEngine() {
@@ -25,11 +32,29 @@ class ClientGame {
 
   initEngine() {
     this.engine.loadSprites(sprites).then(() => {
-      this.engine.on('render', () => {
-        this.map.init();
+      this.map.init();
+      this.engine.on('render', (_, time) => {
+        this.map.render(time);
       });
       this.engine.start();
+      this.initKeys();
     });
+  }
+
+  initKeys() {
+    this.engine.input.onKey({
+      ArrowLeft: (k) => this.movePlayerByDirection(k)(-1, 0),
+      ArrowRight: (k) => this.movePlayerByDirection(k)(1, 0),
+      ArrowUp: (k) => this.movePlayerByDirection(k)(0, -1),
+      ArrowDown: (k) => this.movePlayerByDirection(k)(0, 1),
+    });
+  }
+
+  movePlayerByDirection(keydown) {
+    // Если клавиша не нажата, то ничего не делаем
+    if (!keydown) return () => false;
+    // иначе будем двигать персонажа
+    return (dcol, drow) => this.player.moveByCellCoord(dcol, drow, (cell) => cell.findObjectsByType('grass').length);
   }
 
   static init(cfg) {
